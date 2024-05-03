b1f0df132767c13f1e114f1c50087e11155176d5
NihhZaeyy <nizaralmas11@gmail.com>
2024-05-03 22:59:35 +0300

    first commit

---
+++ flowerish/src/app/components/navigation/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <section className="bg-neutral-600">
+      <div className="p-4">INI bukan NAVBAR</div>
+    </section>
+  );
+};
+
+export default Navbar;
---
+++ flowerish/src/app/page.jsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <main>
+      <div>Hello World</div>
+    </main>
+  );
+};
+
+export default HomePage;
